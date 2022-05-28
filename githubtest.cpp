b887dc3fcb524d4e71e4248c03fa68be92007dd5
Lasha Nikolaishvili <nikolaishvililashan1@gmail.com>
2022-05-28 13:25:50 +0400

    Add a Renctangle

--- githubtest.cpp
+++ githubtest.cpp
@@ -3,4 +3,12 @@ using namespace std;
 
 int main() {
     cout<<"Hello World" <<endl;
+
+    for(int i=0; i<5; i++) {
+        for(int j=0; j<5; j++) {
+            cout<<'*';
+        }
+        cout<<endl;
+    }
+
 }
